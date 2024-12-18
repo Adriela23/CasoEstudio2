194f57d397bfb87f7fc0b50aa4f05e18c8ef0c3e
adriela <you@example.com>
2024-12-17 21:36:56 -0600

    funcion consultar casas agregada en casas model

--- Model/CasasModel.php
+++ Model/CasasModel.php
@@ -1,25 +1,23 @@
 <?php
-require_once 'db.php';
+require_once 'BaseDatos.php';
 
-class CasasModel {
-    public function obtenerCasas() {
-        global $pdo;
-        $stmt = $pdo->prepare("SELECT *, IF(UsuarioAlquiler IS NULL, 'Disponible', 'Reservada') AS Estado FROM CasasSistema WHERE PrecioCasa BETWEEN 115000 AND 180000 ORDER BY Estado ASC");
-        $stmt->execute();
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    function consultarCasas($minPrecio, $maxPrecio)
+    {
+        try{
+            $enlace = AbrirBD();
+
+            $sentencia = "CALL ConsultarCasas($minPrecio, $maxPrecio)";
+            $resultado = $enlace -> query($sentencia);
+
+            CerrarBD($enlace);
+            return $resultado;
+        }
+        catch (Exception $ex)
+        {
+            return null;
+        }
     }
 
-    public function obtenerCasasDisponibles() {
-        global $pdo;
-        $stmt = $pdo->prepare("SELECT IdCasa, DescripcionCasa, PrecioCasa FROM CasasSistema WHERE UsuarioAlquiler IS NULL");
-        $stmt->execute();
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
-    }
 
-    public function alquilarCasa($idCasa, $usuario, $fecha) {
-        global $pdo;
-        $stmt = $pdo->prepare("UPDATE CasasSistema SET UsuarioAlquiler = ?, FechaAlquiler = ? WHERE IdCasa = ?");
-        $stmt->execute([$usuario, $fecha, $idCasa]);
-    }
-}
 ?>
+
